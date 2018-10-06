5ddc874b2840c35415320eeea40da4a244b6811b
Scott Bowler <scott.bowler@learningfuze.com>
2018-10-05 18:21:21 -0700

    Initial routing and components setup

---
+++ src/components/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => (
+    <div>
+        <h1 className="center">Welcome to Movie Quotes</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi labore exercitationem consequuntur iusto ut ex illo adipisci provident itaque, officia deleniti similique tempore reiciendis sed, at rerum laboriosam quae harum?</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur sed quasi earum id sit numquam? Commodi similique culpa qui nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo consequatur iusto eos velit earum voluptate corrupti, minus esse excepturi magni. Ducimus aperiam necessitatibus est unde. Nesciunt maxime quia blanditiis ea deleniti sunt explicabo delectus?</p>
+    </div>
+);
