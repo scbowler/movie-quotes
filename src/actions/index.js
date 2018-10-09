8ada2b5950826c33bae20b610f0dffa873e6e83f
Scott Bowler <scott.bowler@learningfuze.com>
2018-10-09 12:47:37 -0700

    Initial sign up complete, will finish after sign in

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,14 @@
 import types from './types';
 import axios from 'axios';
 
+export const signUp = (userInfo) => {
+    return async (dispatch) => {
+        try {
+            const resp = await axios.post('http://api.reactprototypes.com/signup', userInfo);
+
+            console.log('Sign Up Response:', resp);
+        } catch(err){
+            console.log('Sign Up Error:', err.message);
+        }
+    }
+}
